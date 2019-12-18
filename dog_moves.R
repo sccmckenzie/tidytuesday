6fcbedf520a316bd60182ac835c983b510cd0fbb
Scott McKenzie <scottmckenzie1993@gmail.com>
2019-12-17 21:47:23 -0600

    ggplot

--- dog_moves.R
+++ dog_moves.R
@@ -2,24 +2,32 @@ library(tidyverse)
 library(sp)
 library(sf)
 library(rnaturalearth)
-library(rnaturalearthdata)
-library(maps)
 
-dog_travel <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_travel.csv')
+dog_descriptions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-17/dog_descriptions.csv')
 
 usa <- ne_countries(scale = "medium", returnclass = 'sf', country = "United States of America")
 
-cities <- us.cities %>% 
-  as_tibble() %>% 
+cities <- as_tibble(maps::us.cities) %>% 
   select(city = name, state = country.etc, pop, lat, long) %>% 
   mutate(city = str_remove(city, " [:upper:]+$"))
 
-dog_travel %>% 
-  count(contact_city, contact_state) %>% 
-  left_join(cities)
-
-theme_set(theme_bw())
+dog_pop <- dog_descriptions %>% 
+  filter(str_detect(contact_state, "[:upper:]+")) %>% 
+  transmute(city = str_to_title(contact_city),
+            state = contact_state) %>% 
+  count(city, state) %>% 
+  left_join(cities) %>% 
+  drop_na() %>% 
+  st_as_sf(coords = c("long", "lat"), remove = FALSE, scrs = 4326, agr = "constant")
 
 ggplot(usa) +
-  geom_sf(fill = "cornsilk") +
-  coord_sf(crs = st_crs(2163), xlim = c(-2000000, 2500000), ylim = c(-2300000, 730000))
+  geom_sf(fill = "azure") +
+  geom_sf(aes(size = n), data = dog_pop, show.legend = 'point', color = "#1c69ab") +
+  coord_sf(crs = st_crs(2163), xlim = c(-2000000, 2500000), ylim = c(-2000000, 730000)) +
+  theme(panel.background = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "bottom") +
+  labs(title = "Adoptable Dog Listings",
+       caption = "Source: The Pudding\nPetfinder.com",
+       size = "# of listings")
