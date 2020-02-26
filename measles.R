7881290d5f5c205802c2687a3ee174e1fe0dc6c4
Scott McKenzie <scottmckenzie1993@gmail.com>
2020-02-25 22:10:59 -0600

    finish measles

--- measles.R
+++ measles.R
@@ -10,11 +10,10 @@ texas_measles <- readr::read_csv("https://raw.githubusercontent.com/WSJ/measles-
 
 pal <- c("#b72a77", "#f1b6da", "#aed4ee", "#41a9b5")
 
-get_stamenmap(bbox = c(-98.1, 30.16, -97.6, 30.42), zoom = 12, maptype = "toner-lines") %>% 
+get_stamenmap(bbox = c(-98, 30.16, -97.6, 30.42), zoom = 12, maptype = "toner-lines") %>% 
   ggmap() +
   geom_point(aes(lng, lat), data = filter(texas_measles, county == "Travis"), color = "black", size = 5) +
   geom_point(aes(lng, lat, color = mmr_cat), data = filter(texas_measles, county == "Travis"), size = 4) +
-  # scale_color_viridis_c(option = "plasma", direction = -1) +
   scale_color_manual(values = rev(pal),
                      labels = c(
                        `(0,75]` = "Less than 75%",
@@ -25,10 +24,7 @@ get_stamenmap(bbox = c(-98.1, 30.16, -97.6, 30.42), zoom = 12, maptype = "toner-
   labs(x = "", y = "", title = "Private School Immunization Rates", subtitle = "Austin, Tx") +
   theme_void() +
   theme(text = element_text(family = 'Helvetica'),
-        plot.title.position = element_text(vjust = 0),
-        legend.position = c(-0.05, 1),
+        legend.position = c(-0.03, 1),
         legend.justification = c(0, 1),
         legend.title = element_blank(),
         legend.background = element_rect(fill = "white", linetype = 0))
-
-ggsave("test.png", g, width = 1.75, height = 1.75, units = "in", scale = 1, dpi = "retina")
